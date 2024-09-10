c1d1ac9d3da8139c4243d5312945bf7a97f0a2b0
Maalug Jonathan <jmaalug11@gmail.com>
2024-09-10 07:21:15 +0000

    added UI for the signup page of sellers

---
+++ src/components/tsx/SignUP/BuildUISeller.tsx
@@ -0,0 +1,296 @@
+import { useEffect, useState } from "react";
+import Spinner from "../Spinner";
+import { getBackendUrl } from "../../utils/Utils";
+import Already from "../Already";
+import "../../css/AlreadyClick.css";
+
+interface Props {
+  handleHasCreatedAccountToast: () => void;
+}
+
+const BuildUISeller = ({ handleHasCreatedAccountToast }: Props) => {
+  // State hooks for input fields
+  const [sellerName, setSellerName] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [agreeToTerms, setAgreeToTerms] = useState(false);
+
+  // State hooks for input validation
+  const [isSellerNameValid, setIsSellerNameValid] = useState(true);
+  const [isUsernameValid, setIsUsernameValid] = useState(true);
+  const [isPasswordValid, setIsPasswordValid] = useState(true);
+  const [passwordMatch, setPasswordMatch] = useState(true);
+  const [isFormValid, setIsFormValid] = useState(false);
+
+  // State hooks for touched fields
+  const [touchedSellerName, setTouchedSellerName] = useState(false);
+  const [touchedUsername, setTouchedUsername] = useState(false);
+  const [touchedPassword, setTouchedPassword] = useState(false);
+  const [touchedConfirmPassword, setTouchedConfirmPassword] = useState(false);
+
+  // State hooks for form status and error handling
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [error, setError] = useState("");
+
+  // Check password length
+  useEffect(() => {
+    setIsPasswordValid(password.length >= 8);
+  }, [password]);
+
+  // Compare password and confirm password
+  useEffect(() => {
+    setPasswordMatch(confirmPassword === password);
+  }, [password, confirmPassword]);
+
+  // Check username length
+  useEffect(() => {
+    setIsUsernameValid(username.length >= 8);
+  }, [username]);
+
+  // Check first name length
+  useEffect(() => {
+    setIsSellerNameValid(sellerName.length >= 5);
+  }, [sellerName]);
+
+  // Check if the form is valid whenever inputs change
+  useEffect(() => {
+    const formValid =
+      sellerName.trim() !== "" &&
+      username.trim() !== "" &&
+      isPasswordValid &&
+      passwordMatch &&
+      isSellerNameValid &&
+      isUsernameValid &&
+      agreeToTerms;
+    setIsFormValid(formValid);
+  }, [
+    sellerName,
+    username,
+    isPasswordValid,
+    passwordMatch,
+    agreeToTerms,
+    isSellerNameValid,
+    isUsernameValid,
+  ]);
+
+  // Helper function to disable the default form submission behavior and perform custom behavior
+  const handleSubmit = async (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+    if (isFormValid) {
+      setIsLoading(true);
+      setIsError(false);
+      try {
+        const response = await fetch(`${getBackendUrl()}/users/add-users`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username: username,
+            password: confirmPassword,
+            // email: email,
+            // first_name: firstName,
+            // last_name: lastName,
+          }),
+        });
+
+        const data = await response.json();
+        if (response.ok) {
+          setSellerName("");
+          setUsername("");
+          setPassword("");
+          setConfirmPassword("");
+          setIsFormValid(false);
+          setTouchedPassword(false);
+          setTouchedConfirmPassword(false);
+          setTouchedSellerName(false);
+          setTouchedUsername(false);
+          handleHasCreatedAccountToast();
+        } else {
+          setIsError(true);
+          setError(data["error"]);
+        }
+      } catch (error) {
+        setIsError(true);
+        setError("Sorry, an error has occured, Please try again..");
+        setIsLoading(false);
+      } finally {
+        setIsLoading(false);
+      }
+    } else {
+      alert("Please correct the errors in the form.");
+    }
+  };
+
+  // Helper function to update the agreement state
+  const handleAgreeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAgreeToTerms(e.target.checked);
+  };
+
+  // Helper function to update the password state
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+    setTouchedPassword(true); // Mark password field as touched
+  };
+
+  // Helper function to update the confirm password state
+  const handleConfirmPasswordChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setConfirmPassword(e.target.value);
+    setTouchedConfirmPassword(true); // Mark confirm password field as touched
+  };
+
+  return (
+    <>
+      <div className="sign-up-form-container">
+        <form
+          onSubmit={handleSubmit}
+          className="col g-3 needs-validation"
+          noValidate
+        >
+          {/* Username text input */}
+          <div className="mb-3">
+            <label htmlFor="validationCustom003" className="form-label">
+              Username
+            </label>
+            <input
+              type="text"
+              className={`form-control ${
+                !isUsernameValid && touchedUsername ? "is-invalid" : ""
+              }`}
+              id="validationCustom003"
+              placeholder="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              onBlur={() => setTouchedUsername(true)}
+              required
+            />
+            {!isUsernameValid && touchedUsername && (
+              <div className="invalid-feedback">
+                Username must be at least 8 characters long.
+              </div>
+            )}
+          </div>
+
+          {/* Seller Name text input */}
+          <div className="mb-3">
+            <label htmlFor="validationCustom01" className="form-label">
+              Seller Name
+            </label>
+            <input
+              type="text"
+              className={`form-control ${
+                !isSellerNameValid && touchedSellerName ? "is-invalid" : ""
+              }`}
+              id="validationCustom01"
+              placeholder="Seller Name"
+              value={sellerName}
+              onChange={(e) => setSellerName(e.target.value)}
+              onBlur={() => setTouchedSellerName(true)}
+              required
+            />
+            {!isSellerNameValid && touchedSellerName && (
+              <div className="invalid-feedback">
+                Seller Name must be at least 5 characters long.
+              </div>
+            )}
+          </div>
+
+          {/* Password text input */}
+          <div className="mb-3">
+            <label htmlFor="inputPassword6" className="col-form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              id="inputPassword6"
+              className={`form-control ${
+                !isPasswordValid && touchedPassword ? "is-invalid" : ""
+              }`}
+              aria-describedby="passwordHelpInline"
+              value={password}
+              onChange={(e) => handlePasswordChange(e)}
+              onBlur={() => setTouchedPassword(true)}
+              required
+            />
+            {!isPasswordValid && touchedPassword && (
+              <div className="invalid-feedback">
+                Password must be at least 8 characters long.
+              </div>
+            )}
+          </div>
+
+          {/* Confirm password field */}
+          <div className="mb-3">
+            <label htmlFor="inputPassword7" className="col-form-label">
+              Confirm Password
+            </label>
+            <input
+              type="password"
+              id="inputPassword7"
+              className={`form-control ${
+                !passwordMatch && touchedConfirmPassword ? "is-invalid" : ""
+              }`}
+              aria-describedby="passwordHelpInline"
+              value={confirmPassword}
+              onChange={(e) => handleConfirmPasswordChange(e)}
+              onBlur={() => setTouchedConfirmPassword(true)}
+              required
+            />
+            {!passwordMatch && touchedConfirmPassword && (
+              <div className="invalid-feedback">Passwords do not match.</div>
+            )}
+          </div>
+
+          {/* Agree to terms and conditions */}
+          <div className="col-12">
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                checked={agreeToTerms}
+                onChange={handleAgreeChange}
+                id="invalidCheck"
+                required
+              />
+              <label className="form-check-label" htmlFor="invalidCheck">
+                Agree to terms and conditions
+              </label>
+              {!agreeToTerms && (
+                <div className="invalid-feedback">
+                  You must agree before submitting.
+                </div>
+              )}
+            </div>
+          </div>
+          <br />
+
+          {/* Submit button */}
+          {isLoading ? (
+            <Spinner />
+          ) : (
+            <button
+              className="btn btn-primary"
+              type="submit"
+              disabled={!isFormValid}
+            >
+              Submit form
+            </button>
+          )}
+          {isError && <p className="error">{error}</p>}
+        </form>
+        <Already
+          title={"Already Have a Sellers Account?"}
+          message={"Click the button below to log in."}
+          buttonText={"Sellers login"}
+          route={"/login-seller"}
+        />
+      </div>
+    </>
+  );
+};
+
+export default BuildUISeller;
--- src/pages/signup/SignUpSeller.tsx
+++ src/pages/signup/SignUpSeller.tsx
@@ -1,5 +1,49 @@
-const SignUpSeller = () => {
-  return <div>SignUpSeller</div>;
+import Footer from "../../components/tsx/Footer";
+import Header from "../../components/tsx/Header";
+import "../../components/css/SignUpPage.css";
+import ToastComponent from "../../components/tsx/Toast";
+import { useState } from "react";
+import { useUser } from "../../UserProvider";
+import Already from "../../components/tsx/Already";
+import BuildUISeller from "../../components/tsx/SignUP/BuildUISeller";
+
+const SignUpPage = () => {
+  const [hasCreatedAccount, setHasCreatedAccount] = useState(false);
+
+  const handleHasCreatedAccountToast = () => {
+    setHasCreatedAccount(true);
+  };
+  const { user } = useUser();
+  const title = "Not Loggen in User Account";
+  const message =
+    "Sorry you can only create an account if you have a user account!";
+  const buttonText = "Go to register";
+  const route = "/signup";
+
+  return (
+    <>
+      <Header pageName={"Sign up Sellers"} pageLink={"/signup-seller"} />
+      {!user ? (
+        <Already
+          title={title}
+          message={message}
+          buttonText={buttonText}
+          route={route}
+        />
+      ) : (
+        <BuildUISeller
+          handleHasCreatedAccountToast={handleHasCreatedAccountToast}
+        />
+      )}
+      {hasCreatedAccount && (
+        <ToastComponent
+          title={"Success"}
+          message={"Your Seller Account has been created.."}
+        />
+      )}
+      <Footer />
+    </>
+  );
 };
 
-export default SignUpSeller;
+export default SignUpPage;
